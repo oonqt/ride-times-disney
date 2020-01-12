2ed6ade726a0188c7fcb7c76042d45dafa7a04b7
oonqt <r0blox1053@outlook.com>
2020-01-12 12:55:30 -0800

    added support for boomer browsers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
+// for boomer browsers
+import "react-app-polyfill/ie9"
+import "react-app-polyfill/ie11"
+import "react-app-polyfill/stable"
+import 'raf/polyfill';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
